593d59a887e7baaa10cb8f942e0bdd971b7fd6d6
Shweta2105 <sghadi21@gmail.com>
2021-06-17 22:43:42 +0530

    UC2-simple pincode if starts with alphbet

--- pincodeCheck.java
+++ pincodeCheck.java
@@ -1,18 +1,21 @@
 package regex.regularExpressions;
+/*
+ * @Author Shweta
+ * @param testPincodeValidation method testing valid pincode
+ */
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class pincodeCheck {
-	
+
 	@Test
 	public void testPincodeValidation() {
 		PincodeValidation pin = new PincodeValidation();
-		
+
 		assertTrue(pin.isPincodeValid("400088"));
-		
-		
+		assertFalse(pin.isPincodeValid("A400088"));
 	}
 
 }
