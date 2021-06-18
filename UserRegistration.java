caa0b8a2f053bfa470a82151e9bbdd97532035b8
Shweta2105 <sghadi21@gmail.com>
2021-06-18 22:31:22 +0530

    UC4- check mobile number

--- UserRegistration.java
+++ UserRegistration.java
@@ -13,10 +13,20 @@ public class UserRegistration {
 		String lastName = scan.nextLine();
 		System.out.println("Enter the last name : ");
 		String email = scan.nextLine();
+		System.out.println("Enter the mobile number : ");
+		String mob = scan.nextLine();
+		
 		System.out.println("Is User First name valid? : "+validateFirstName(firstName));
 		System.out.println("Is user Last name valid? : "+validateLastName(lastName));
 		System.out.println("Is user email valid? : "+validateEmail(email));
+		System.out.println("Is user mobile number valid? : "+validateMobile(mob));
 		
+		
+	}
+
+	private static boolean validateMobile(String mob) {
+		
+		return Pattern.matches("^([0-9]{0,1})\s([6-9]{1}[0-9]{9})$", mob);
 	}
 
 	private static boolean validateEmail(String email) {
