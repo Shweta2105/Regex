c3b664c74035d2419ada21f72e0aa4a4b4cbe3ce
Shweta2105 <sghadi21@gmail.com>
2021-06-18 09:27:05 +0530

    UC5-valid emails are checked

--- EmailValidationTest.java
+++ EmailValidationTest.java
@@ -15,6 +15,15 @@ public class EmailValidationTest {
 	EmailValidation email = new EmailValidation();
 	
 	assertTrue(email.isEmailValid("abc.xyz@bridgelabz.co"));
+	assertTrue(email.isEmailValid("abc.100@yahoo.com"));
+	assertTrue(email.isEmailValid("abc112@gmail.net"));
+	assertTrue(email.isEmailValid("abc@1.com.au"));
+	assertFalse(email.isEmailValid("abc()*@bridgelabz.co"));
+	assertFalse(email.isEmailValid("abc@abc@gmail.co"));
+	assertFalse(email.isEmailValid("#abcd.def92@3452.abcd"));
+	assertFalse(email.isEmailValid("ab$c92@.yahoo.com.ina"));
+	assertFalse(email.isEmailValid("abc*xyz@bridgelabz.co.in"));
+		
 	
 	}
 }
