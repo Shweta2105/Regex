118215fd0467d6f9380b5c2e31c25040b2fe96a2
Shweta2105 <sghadi21@gmail.com>
2021-06-18 22:03:29 +0530

    UC2- check last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,9 +9,15 @@ public class UserRegistration {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the first name : ");
 		String firstName = scan.nextLine();
-		
+		System.out.println("Enter the last name : ");
+		String lastName = scan.nextLine();
 		System.out.println("Is User First name valid? : "+validateFirstName(firstName));
+		System.out.println("Is user Last name valid? : "+validateLastName(lastName));
 		
+	}
+
+	private static boolean validateLastName(String lastName) {
+		return Pattern.matches("^[A-Z][a-z]{3,15}$", lastName);
 	}
 
 	private static boolean validateFirstName(String firstName) {
