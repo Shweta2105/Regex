27d46d5a305fab75f8752bf974e9b56b7a0edc7b
Shweta2105 <sghadi21@gmail.com>
2021-06-18 23:36:05 +0530

    UC5- password rule 1 checked

--- UserRegistration.java
+++ UserRegistration.java
@@ -11,22 +11,30 @@ public class UserRegistration {
 		String firstName = scan.nextLine();
 		System.out.println("Enter the last name : ");
 		String lastName = scan.nextLine();
-		System.out.println("Enter the last name : ");
+		System.out.println("Enter the email id : ");
 		String email = scan.nextLine();
 		System.out.println("Enter the mobile number : ");
 		String mob = scan.nextLine();
+		System.out.println("Enter the password : ");
+		String password = scan.nextLine();
 		
 		System.out.println("Is User First name valid? : "+validateFirstName(firstName));
 		System.out.println("Is user Last name valid? : "+validateLastName(lastName));
 		System.out.println("Is user email valid? : "+validateEmail(email));
 		System.out.println("Is user mobile number valid? : "+validateMobile(mob));
+		System.out.println("Is user password valid? : "+validatePassword(password));
 		
 		
 	}
 
+	private static boolean validatePassword(String password) {
+		
+		return Pattern.matches("^([a-z]){8,25}$", password);
+	}
+
 	private static boolean validateMobile(String mob) {
 		
-		return Pattern.matches("^([0-9]{0,1})\s([6-9]{1}[0-9]{9})$", mob);
+		return Pattern.matches("^\\d{2}[ ]\\d{10}$", mob);
 	}
 
 	private static boolean validateEmail(String email) {
