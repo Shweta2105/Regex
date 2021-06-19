242bc9eecb16619e3cb91469093c91716aaf3589
Shweta2105 <sghadi21@gmail.com>
2021-06-19 09:23:47 +0530

    UC8- password rule 4 checked

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,7 +2,10 @@ package regex.regularExpressions;
 
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
+/*
+ * @authorShweta
+ * Day 19 Assignment User registration Problem
+ */
 public class UserRegistration {
 	
 	public static void main(String[] args) {
@@ -27,26 +30,45 @@ public class UserRegistration {
 		
 	}
 
-	private static boolean validatePassword(String password) {
+	private static boolean validatePassword(String password) { //UC8-> UC7-> UC6->UC5 password validation
+		/*@param password
+		 * @ return boolean
+		 */
 		
 		return Pattern.matches("^(?=.*?[0-9a-zA-Z])[0-9a-zA-Z]*[@#$%][0-9a-zA-Z]*$", password);
 	}
 
-	private static boolean validateMobile(String mob) {
+	private static boolean validateMobile(String mob) {	//UC4 mobile number validation
+		/*@param mobile
+		 * @ return boolean
+		 */
 		
 		return Pattern.matches("^\\d{2}[ ]\\d{10}$", mob);
 	}
 
-	private static boolean validateEmail(String email) {
+	private static boolean validateEmail(String email) {	//UC3 email validation
+		/*@param Email
+		 * @ return boolean
+		 */
+		
 		return Pattern.matches("^([a-zA-Z0-9.]+)([0-9a-zA-Z]{0,1}@([a-zA-Z0-9.]+([a-zA-Z]{2,3}))+([a-z]{0,2}))$", email);
 
 	}
 
-	private static boolean validateLastName(String lastName) {
+	private static boolean validateLastName(String lastName) { //UC2 Last name validation
+		/*@param Last name
+		 * @ return boolean
+		 */
+		
 		return Pattern.matches("^[A-Z][a-z]{3,15}$", lastName);
 	}
 
-	private static boolean validateFirstName(String firstName) {
+	private static boolean validateFirstName(String firstName) { //UC1 firstname validation
+		
+		/*@param first name
+		 * @ return boolean
+		 */
+		
 		return Pattern.matches("^[A-Z][a-z]{3,15}$", firstName);
 		
 		
